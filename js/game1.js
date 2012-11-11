597f9f2cff21317dfdd51f1aab3bcfb7211d1915
Lech Stawikowski <lech.stawikowski@gmail.com>
2012-11-11 18:02:49 +0100

    edycja zdjecia gry
    Dodane pola: startowe, koncowe, z pionkiem i te, na ktore pionek moze
    stanac. Tymczasem takie zostanie "zdjecie" gry pojawiajace sie po
    wybraniu gry z menu glownego.

--- js/game1.js
+++ js/game1.js
@@ -1,7 +1,11 @@
 var gMML = '<div class="chessboard">';
 
 for(var i=0; i < 64; i++) {
-    gMML=gMML+'<div class="field"></div>'
+    if(i==27) {
+        gMML=gMML+'<div class="field field'+i+'"><div class="pawn"></div></div>';
+    } else {
+        gMML=gMML+'<div class="field field'+i+'"></div>';
+    }
 }
 
 gMML = gMML + '</div>';

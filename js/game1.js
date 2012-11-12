280575e47c63ae4e928ba69bb12d9e5239e9605c
Lech Stawikowski <lech.stawikowski@gmail.com>
2012-11-12 20:42:39 +0100

    instrukcja - wersja wstepna
    Poprawiona wersja instrukcji do gry. Jest to wstepna wersja, ale na
    razie taka zostanie, a ja przechodze do nastepnego kroku, czyli do
    wlasciwej gry.

--- js/game1.js
+++ js/game1.js
@@ -10,9 +10,47 @@ for(var i=0; i < 64; i++) {
 
 gMML = gMML + '</div>';
 
+var gMML0 = '<div class="chessboard">';
+
+for(var i=0; i < 64; i++) {
+    if(i==0) {
+        gMML0=gMML0+'<div class="field field'+i+'"><div class="pawn"></div></div>';
+    } else {
+        gMML0=gMML0+'<div class="field"></div>';
+    }
+}
+
+gMML0 = gMML0 + '</div>';
+
+var gMML1 = '<div class="chessboard">';
+
+for(var i=0; i < 64; i++) {
+    if(i==27) {
+        gMML1=gMML1+'<div class="field field'+i+'"><div class="pawn"></div></div>';
+    } else if(i==28 || i==34 || i==35 || i==36) {
+        gMML1=gMML1+'<div class="field field'+i+'"></div>';
+    } else {
+        gMML1=gMML1+'<div class="field"></div>';
+    }
+}
+
+gMML1 = gMML1 + '</div>';
+
+var gMML2 = '<div class="chessboard">';
+
+for(var i=0; i < 64; i++) {
+    if(i==63) {
+        gMML2=gMML2+'<div class="field field'+i+'"><div class="pawn"></div></div>';
+    } else {
+        gMML2=gMML2+'<div class="field"></div>';
+    }
+}
+
+gMML2 = gMML2 + '</div>';
+
 var game1 = {name: 'Pawn On a Chessboard',
              gameMainMenuLayout: gMML,
-             gameIntro: ["Strona 1"+gMML, "Strona 2"+gMML, "Strona 3"+gMML],
+             gameIntro: ["Starting from the green field..."+gMML0, "...and making one of the possible moves..."+gMML1, "...you have to reach the red field."+gMML2],
              gameIntroLayout: function() {
                  mainLayout(appName+': '+game1.name, "Please read the instructions, then click Play",
                      '<div id="intro"></div><div id="introbuttons"><input id="introback" type="button" value="Back" /><input id="intronext" type="button" value="Next" /></div>',
